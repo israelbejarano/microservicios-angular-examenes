7198f1089cbc8134e860922b2d899960a6f84bc6
israel <israel.bejarano.a@gmail.com>
2020-04-26 12:44:27 +0200

    examen service

---
+++ microservicios-examenes/src/main/java/com/curso/microservicios/app/examenes/services/ExamenService.java
@@ -0,0 +1,8 @@
+package com.curso.microservicios.app.examenes.services;
+
+import com.curso.microservicios.app.examenes.models.entity.Examen;
+import com.curso.microservicios.commons.services.CommonService;
+
+public interface ExamenService extends CommonService<Examen> {
+
+}
