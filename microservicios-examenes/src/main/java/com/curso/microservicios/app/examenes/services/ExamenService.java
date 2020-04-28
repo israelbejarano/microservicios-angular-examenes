e15d21eacb0842d34ce170b04f70e2ed10015167
israel <israel.bejarano.a@gmail.com>
2020-04-28 11:20:42 +0200

    buscar examen por nombre

--- microservicios-examenes/src/main/java/com/curso/microservicios/app/examenes/services/ExamenService.java
+++ microservicios-examenes/src/main/java/com/curso/microservicios/app/examenes/services/ExamenService.java
@@ -1,8 +1,12 @@
 package com.curso.microservicios.app.examenes.services;
 
+import java.util.List;
+
 import com.curso.microservicios.commons.examenes.models.entity.Examen;
 import com.curso.microservicios.commons.services.CommonService;
 
 public interface ExamenService extends CommonService<Examen> {
+	
+	public List<Examen> findByNombre(String term);
 
 }
--- microservicios-examenes/src/main/java/com/curso/microservicios/app/examenes/services/ExamenServiceImpl.java
+++ microservicios-examenes/src/main/java/com/curso/microservicios/app/examenes/services/ExamenServiceImpl.java
@@ -1,7 +1,10 @@
 package com.curso.microservicios.app.examenes.services;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.microservicios.app.examenes.models.repository.ExamenRepository;
 import com.curso.microservicios.commons.examenes.models.entity.Examen;
@@ -9,6 +12,12 @@ import com.curso.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class ExamenServiceImpl extends CommonServiceImpl<Examen, ExamenRepository> implements ExamenService{
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Examen> findByNombre(String term) {
+		return repository.findByNombre(term);
+	}
 
 
 }
